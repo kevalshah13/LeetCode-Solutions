114e2a4b649808c8c6ccc3cbcc23497a2ea8c772
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-02-01 21:25:01 -0700

    Time: 30 ms (87.58%) | Memory: 16.5 MB (72.98%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -2,36 +2,12 @@ class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
         ans=[]
         for i in range(1,10):
-            num=i
-            rem=i
+            num,rem=i,i
             while(num<=high and rem<10):
                 if(num>=low and num<=high):
                     ans.append(num)
                 num=(num*10)+(rem+1)
                 rem+=1
-                
-            # print(ans)
         return sorted(ans)
-        
-        
-        
-        # t=str(low)[0]
-        # n=len(str(low))
-        # for i in range(1,n):
-        #     t+=str(int(t[i-1])+1)
-        # print(t)
-        # ans=[int(t)]
-        # while(int(t)<=high):
-
-
-        # while(int(t)<=high):
-        #     while(len(t)<len(str(high))):
-        #         t+=str(int(t[-1])+1)
-        #         print(t,'11')
-        #     t=t[:-1]
-        #     t=t[1:]+str(int(t[-1])+1)
-        #     print(t)
-        #     ans.append(int(t))
-        # return ans
         
     

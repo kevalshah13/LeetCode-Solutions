7dbb8c99442541e6796c35e8c3254097b6a2eafb
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-02-04 18:09:48 -0700

    Time: 93 ms (62.33%) | Memory: 16.8 MB (66.48%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -4,11 +4,11 @@ class Solution(object):
         a=-1
         for i in s:
             mem[i]=1+mem.get(i,0)
-        for i in mem:
-            if mem[i]==1:
+        for i in range(len(s)):
+            if mem[s[i]]==1:
                 a=i
                 break
-        if a==-1:
-            return a
-        else:
-            return s.index(a)
+        # if a==-1:
+        return a
+        # else:
+            # return s.index(a)

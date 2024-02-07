47a088ced1ac4a33f2326ecac2b12aed8256a32d
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-02-07 11:58:06 -0700

    Time: 71 ms (16.16%) | Memory: 26.1 MB (9.33%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -3,18 +3,14 @@ class Solution:
         count={}
         for i in s:
             count[i]=1+count.get(i,0)
-        # print(count)
         sort = [[] for _ in range(len(s) + 1)]
         for i,n in count.items():
             sort[n].append(i)
-        # print(sort)
         ans=''
         for i in range(len(sort)-1,0,-1):
             j=0
             while(sort[i]):
                 ans+=sort[i][j]*i
                 del sort[i][j]
-        # print(ans)
-        # print(sort)
         return ans
         

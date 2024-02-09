7ce93cb189c301556c8961f12ff3bc8f2f16ecd2
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-02-08 18:57:18 -0700

    Time: 226 ms (53.13%) | Memory: 16.6 MB (98.50%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -6,7 +6,6 @@ class Solution:
             for j in range(i-1,-1,-1):
                 if(nums[i]%nums[j]==0):
                     dp[i]=max(dp[i],1+dp[j])
-        # print(dp)
         m=max(dp)
         prev=dp.index(m)
         ans=[]
@@ -15,34 +14,7 @@ class Solution:
                 ans.append(nums[i])
                 m-=1
                 prev=i
-        # print(ans)
-
         return ans
-        # l,r=0,0
-        # while(r<len(nums)):
-        #     while(l<r):
-        #         if(nums[l]%nums[r]==0 or nums[r]%nums[l]==0):
-        #             dp[nums[r]].append(nums[l])
-        #         l+=1
-        #     print(l,r)
-        #     r+=1
-        #     l=0
-            
-        
-        print(dp)
-        # for i in nums:
-        #     for j in range(1,i+1):
-        #         if(i%j==0 or j%i==0):
-        #             dp[i].append(j)
-        # print(dp)
-        # ans=0
-        # m=0
-        # for i in nums:
-        #     if(len(dp[i])>m):
-        #         ans=dp[i]
-        #         m=len(dp[i])
-        # print(ans)
-        return []
 
 
 

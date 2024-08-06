7290b2712c849f07fe386d2e9052e694f39a5e9e
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-08-05 17:15:09 -0700

    Time: 213 ms (36.52%) | Memory: 17.7 MB (36.75%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        available=8
+        mem={}
+        for i in word:
+            mem[i]=1+mem.get(i,0)
+        if(len(mem)<=8):
+            return len(word)
+        mem = dict(sorted(mem.items(), key=lambda x: x[1],reverse=True))
+        ans=0
+        count=0
+        mult=1
+        for i in mem:
+            if(count>=8):
+                count=count%8
+                mult+=1
+            ans+=mem[i]*mult
+            count+=1
+        return ans

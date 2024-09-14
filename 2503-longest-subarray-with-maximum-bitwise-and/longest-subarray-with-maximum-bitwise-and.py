e24fce2e9d00ddc486c6e5376749a026d1b716c7
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-09-14 16:06:48 -0700

    Time: 661 ms (47.54%) | Memory: 31.2 MB (41.20%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -1,17 +1,29 @@
+# class Solution:
+#     def longestSubarray(self, nums: List[int]) -> int:
+#         mem=0
+#         m=max(nums)
+#         if(len(nums)<2):
+#             return 1
+#         l,r=0,1
+#         while(r<len(nums)):
+#             while(r<len(nums) and nums[l]==m and nums[l]==nums[r]):
+#                 r+=1
+#             mem=max(mem,r-l)
+#             l=r
+#             r+=1
+#         return mem
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        mem=0
-        m=max(nums)
-        if(len(nums)<2):
-            return 1
-        l,r=0,1
-        while(r<len(nums)):
-            # curr=1
-            while(r<len(nums) and nums[l]==m and nums[l]==nums[r]):
-                # curr+=1
-                r+=1
-            mem=max(mem,r-l)
-            # print(l,r,mem)
-            l=r
-            r+=1
-        return mem
+        max_val = ans = current_streak = 0
+        for num in nums:
+            if max_val < num:
+                max_val = num
+                ans = current_streak = 0
+
+            if max_val == num:
+                current_streak += 1
+            else:
+                current_streak = 0
+
+            ans = max(ans, current_streak)
+        return ans

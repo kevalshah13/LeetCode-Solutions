ad7fcd29f677122a3897b8455aa08794548ac8b2
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-09-16 11:48:10 -0700

    Time: 60 ms (93.49%) | Memory: 19.8 MB (20.92%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.py
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -9,12 +9,6 @@ class Solution:
         ans=24*60
         for i in range(1,len(new)):
             diff=new[i]-new[i-1]
-        #     # if(new[i][0]<=12):
-        #     diff=(new[i][0]-new[i-1][0])*60+(new[i][1]-new[i-1][1])
-        #     # if(diff>=12*60):
-        #     #     diff=abs(1440-diff)
             ans=min(ans,diff)
-        #     print(ans)
-        # ans=min(ans,abs((new[0][0]-new[len(new)-1][0])*60+(new[0][1]-new[len(new)-1][1])))
         return min(ans,24*60-new[-1]+new[0])
         

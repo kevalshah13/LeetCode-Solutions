f2bfee5440dbdebb4b38699f3f019af6ae12eb21
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-10-04 18:01:46 -0700

    Time: 60 ms (58.76%) | Memory: 17.8 MB (11.47%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mem={}
-        for i,n in enumerate(nums):
-            diff=target-n
-            if diff in mem:
+        for i in range(len(nums)):
+            diff=target-nums[i]
+            if(diff in mem):
                 return [mem[diff],i]
-            else:
-                mem[n]=i
-
+            mem[nums[i]]=i

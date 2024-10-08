aa5f0bca137dbaad3992f6fa6703912eb9e4a054
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-10-08 15:53:49 -0700

    Time: 122 ms (42.06%) | Memory: 23.3 MB (67.67%) - LeetSync

---
+++ 359-logger-rate-limiter/logger-rate-limiter.py
@@ -0,0 +1,20 @@
+class Logger:
+
+    def __init__(self):
+        self.map={}
+
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if(message in self.map):
+            if(timestamp>=self.map[message]):
+                self.map[message]=timestamp+10
+                return True
+            else:
+                return False
+        self.map[message]=timestamp+10
+        return True
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)

ecf91f995a2f64ce0621cf6a2f414d4fe771d226
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2024-10-11 14:53:11 -0700

    Time: 39 ms (68.75%) | Memory: 16.8 MB (63.82%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -3,7 +3,8 @@ class Solution:
         citations.sort()
         ans=0
         for i in range(len(citations)-1,-1,-1):
-            if(citations[i]>=len(citations)-i):
-                ans=max(ans,len(citations)-i)
+            if(citations[i]<len(citations)-i):
+                break
+            ans=max(ans,len(citations)-i)
         return ans            
         

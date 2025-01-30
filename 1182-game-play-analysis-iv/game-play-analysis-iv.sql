cb879ff5ddd6b14fd680afbe367f4deee43a1c4c
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2025-01-30 16:04:43 -0700

    Time: 1278 ms (22.29%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,25 +1,28 @@
 # Write your MySQL query statement below
-SELECT 
-    ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
-FROM 
-    Activity
-WHERE 
-    (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
-    IN 
-    (
-        -- Subquery: Players and their first login date
-        SELECT 
-            player_id,
-            MIN(event_date) AS first_login
-        FROM 
-            Activity
-        GROUP BY 
-            player_id
-    );
--- with table1 as
--- (select player_id,event_date, lag(event_date) over(order by event_date) as prev_date,
--- dense_rank() over(partition by player_id order by event_date) as ro
--- from activity)
--- select Round(sum(case when (event_date-prev_date=1 or prev_Date is Null) and ro=2 then 1 else 0 end)/count(distinct player_id),2) as fraction
--- from table1
+-- with temp as
+-- (select player_id, 
+-- event_date,
+-- timestampdiff(day,event_date,lag(event_date) over(order by event_date)) as ddiff
+-- from activity 
+-- group by player_id
+-- having event_date=min(event_date) and ddiff=1)
+-- select Round(count(*)/(select count(distinct player_id) from activity),2) as fraction
+-- from temp;
 
+SELECT
+  ROUND(
+    COUNT(A1.player_id)
+    / (SELECT COUNT(DISTINCT A3.player_id) FROM Activity A3)
+  , 2) AS fraction
+FROM
+  Activity A1
+WHERE
+  (A1.player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN (
+    SELECT
+      A2.player_id,
+      MIN(A2.event_date)
+    FROM
+      Activity A2
+    GROUP BY
+      A2.player_id
+  );

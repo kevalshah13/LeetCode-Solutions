5ab301f13e4a841727b3ec9035ce80cbdfbf28f7
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2025-01-30 14:13:04 -0700

    Time: 1628 ms (24.68%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
-select a.product_id, coalesce(Round(sum(a.price*b.units)/sum(b.units),2),0) as average_price
-from Prices a left join UnitsSold b 
-on a.product_id=b.product_id and b.purchase_date between a.start_date and a.end_date
-group by a.product_id
+-- with TotalUnit as 
+-- (select sum(units) as total_units
+-- from UnitsSold
+-- group by product_id)
+with agg as
+(select p.product_id,units,price
+from prices p left join unitsSold us
+on p.product_id=us.product_id and p.start_date<=us.purchase_Date and us.purchase_date<=p.end_date)
+select product_id,
+round(coalesce(sum(price*units)/sum(units),0.0),2) as average_price
+from agg
+group by product_id;

f339b1bec0d6f0956e326dc691ab550a89651f12
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2025-02-01 00:49:43 -0700

    Time: 1047 ms (88.18%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,7 +1,17 @@
-# Write your MySQL query statement below
-select product_id, first_year, quantity, price
-from (
-select b.product_id, a.year as first_year, a.quantity, a.price,rank() over(partition by a.product_id order by a.year) as r
-from sales a inner join product b
-on a.product_id=b.product_id) bc
-where r=1
+SELECT 
+  product_id, 
+  year AS first_year, 
+  quantity, 
+  price 
+FROM 
+  Sales 
+WHERE 
+  (product_id, year) IN (
+    SELECT 
+      product_id, 
+      MIN(year) AS year 
+    FROM 
+      Sales 
+    GROUP BY 
+      product_id
+  );

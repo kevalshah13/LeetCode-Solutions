de54a48fd39efcbcde704265532f9d36b259d68d
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2025-02-01 01:18:09 -0700

    Time: 1166 ms (28.19%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -7,4 +7,3 @@ select customer_id
 from temp
 group by customer_id
 having count(distinct product_key)=(select count(distinct product_key) from product);
--- where product_key in (select distinct product_key from product);

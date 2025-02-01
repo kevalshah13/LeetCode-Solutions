1b3b3162965cc219fb16a46c073897342679f212
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2025-02-01 01:16:35 -0700

    Time: 1350 ms (16.14%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,5 +1,10 @@
 # Write your MySQL query statement below
+with temp as 
+(select distinct customer_id,p.product_key
+from customer c ,product p
+where p.product_key=c.product_key)
 select customer_id
-from customer c 
+from temp
 group by customer_id
-having count(distinct product_key) = (select count(product_key) from product)
+having count(distinct product_key)=(select count(distinct product_key) from product);
+-- where product_key in (select distinct product_key from product);

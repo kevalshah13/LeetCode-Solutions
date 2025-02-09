6118ead604d02bd224709043688d5f902b155666
kevalshah13 <58288944+kevalshah13@users.noreply.github.com>
2025-02-09 14:25:14 -0700

    Time: 32 ms (88.92%) | Memory: 17.7 MB (67.44%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,11 +1,14 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        i1,i2=0,0
         ans=''
-        i=0
-        while(i<len(word1) and i<len(word2)):
-            ans+=word1[i]+word2[i]
-            i+=1
-        # if(word1):
-        ans+=word1[i:] or word2[i:]
+        while(i1<len(word1) and i2<len(word2)):
+            ans+=word1[i1]
+            ans+=word2[i2]
+            i1+=1
+            i2+=1
+        if i1<len(word1):
+            ans+=word1[i1:]
+        if i2<len(word2):
+            ans+=word2[i2:]
         return ans
-        
